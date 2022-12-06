d9debde9e4cee09a9e9ad4098232e6751c1636d7
chientqph25370 <chientqph25370@fpt.edu.vn>
2022-12-07 01:47:37 +0700

    12

--- src/Utility/DBConnect.java
+++ src/Utility/DBConnect.java
@@ -17,7 +17,7 @@ public class DBConnect {
     public static final String PORT = "1433";
     public static final String DBNAME = "DUAN_1";
     public static final String USERNAME = "sa";
-    public static final String PASSWORD = "123456";
+    public static final String PASSWORD = "123";
 
     public static Connection getConnection() {
 
